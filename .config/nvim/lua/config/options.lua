988cfac8c0dd0ac0f6ecee6e9dc6a5be1e530bbd
Braden Griebel <bgriebel@uw.edu>
2025-07-14 11:33:29 -0700

    Changed slime config to lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,16 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- slime for tmux
-vim.g.slime_target = "tmux"
--- vim.g.slime_python_ipython = 1
-vim.cmd([[
-  let g:slime_dont_ask_default = 1
-  let g:slime_default_config = {"socket_name":get(split($TMUX, ","), 0), "target_pane": ":.2"}
-]])
-
 -- Set the python provider
-vim.cmd("let g:python3_host_prog = '/home/bgriebel/environments/pynvim/bin/python'")
+vim.g.python3_host_prog = "/home/bgriebel/environments/pynvim/bin/python"
 
 -- Stop autocommenting next line
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
--- .config/nvim/lua/plugins/slime.lua
+++ .config/nvim/lua/plugins/slime.lua
@@ -2,5 +2,20 @@ return {
   -- add vim slime
   {
     "jpalardy/vim-slime",
+    init = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_bracketed_paste = 1
+      vim.g.slime_default_config = {
+        socket_name = vim.fn.split(vim.env.TMUX, ",")[1],
+        target_pane = ":.2",
+      }
+    end,
+    config = function()
+      vim.g.slime_menu_config = false
+      vim.keymap.set("n", "gz", "<Plug>SlimeMotionSend", { remap = true, silent = false })
+      vim.keymap.set("n", "gzz", "<Plug>SlimeLineSend", { remap = true, silent = false })
+      vim.keymap.set("x", "gz", "<Plug>SlimeRegionSend", { remap = true, silent = false })
+      vim.keymap.set("n", "gzc", "<Plug>SlimeConfig", { remap = true, silent = false })
+    end,
   },
 }
